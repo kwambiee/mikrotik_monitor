2946fb4dc5ae04b5f4f6f5670094b444e50a7619
hillarioh <okerioh@gmail.com>
2025-04-04 17:51:35 +0300

    Update ALLOWED_HOSTS for local development and add redirect to router URLs

--- mikrotik_monitor/urls.py
+++ mikrotik_monitor/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include 
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('routers.urls')),
+    
+    # redirect to base URL
+    path('', RedirectView.as_view(url='/routers/', permanent=True)),
 ]
